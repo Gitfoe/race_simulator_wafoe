35eab487dc1e210bffde0b3e8fcf706dea29b8f8
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2021-09-15 18:43:31 +0200

    Level 2-1

---
+++ Model/Interfaces/IEquipment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Interfaces
+{
+    interface IEquipment
+    {
+        int Quality { get; }
+        int Performance { get; }
+        int Speed { get; }
+        bool IsBroken { get; }
+    }
+}
---
+++ Model/Interfaces/IParticipant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Interfaces
+{
+    interface IParticipant
+    {
+        string Name { get; }
+        int Points { get; }
+        IEquipment Equipment { get; }
+        TeamColors TeamColor { get; }
+    }
+    enum TeamColors { Red, Green, Yellow, Grey, Blue };
+}
