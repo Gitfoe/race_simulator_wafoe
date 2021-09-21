b7f3454623b6a131035f5f7fb63acff6d1741b86
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2021-09-21 13:30:46 +0200

    Level 3-3

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using Model;
 using Model.Classes;
+using static Model.Classes.Section;
 using Model.Interfaces;
 using Controller;
 using Controller.Classes;
@@ -30,5 +31,24 @@ namespace ControllerTest
             var result = _competition.NextTrack();
             Assert.IsNull(result);
         }
+
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            Track _track = new Track("Test Track", new Section.SectionTypes[] {
+                    SectionTypes.LeftCorner,
+                    SectionTypes.StartGrid,
+                    SectionTypes.LeftCorner,
+                    SectionTypes.Finish,
+                    SectionTypes.LeftCorner,
+                    SectionTypes.StartGrid,
+                    SectionTypes.LeftCorner,
+                    SectionTypes.StartGrid });
+            Competition _competition = new Competition();
+            _competition.Tracks.Enqueue(_track);
+            var result = _competition.NextTrack();
+            Assert.AreEqual(result, _track);
+        }
+        
     }
 }
