497e7e4ae813fa30b038c1844cfd91273fb16345
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2021-09-21 14:51:59 +0200

    Level 4.2 (graphics)

---
+++ View/Classes/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace View.Classes
+{
+    public static class Visualisation
+    {
+        // Properties
+        public static string[] MyProperty { get; set; }
+
+        // Methods
+        public static void Initiliaze()
+        {
+
+        }
+
+        public static void DrawTrack()
+        {
+
+        }
+
+        // Region
+        #region graphics
+        private static string[] _startGridHorizontal = { "----",
+                                                         "  > ", 
+                                                         " >  ",
+                                                         "----" };
+        private static string[] _startGridVertical =   { "|  |",
+                                                         "|^ |",
+                                                         "| ^|",
+                                                         "|  |" };
+        private static string[] _finishHorizontal =    { "----",
+                                                         "  # ",
+                                                         "  # ",
+                                                         "----" };
+        private static string[] _finishVertical =      { "|  |",
+                                                         "|##|",
+                                                         "|  |",
+                                                         "|  |" };
+        private static string[] _straightHorizontal =  { "----",
+                                                         "    ",
+                                                         "    ",
+                                                         "----" };
+        private static string[] _straightVertical =    { "|  |",
+                                                         "|  |",
+                                                         "|  |",
+                                                         "|  |" };
+        private static string[] _rightCornerDown =     {@"--\ ",
+                                                        @"   \",
+                                                         "   |",
+                                                        @"\  |" };
+        private static string[] _leftCornerDown =      {@" /--",
+                                                        @"/   ",
+                                                         "|   ",
+                                                        @"|  /" };
+        private static string[] _rightCornerUp =       {@"|  \",
+                                                         "|   ",
+                                                        @"\   ",
+                                                        @" \--" };
+        private static string[] _leftCornerUp =        {@"/  |",
+                                                         "   |",
+                                                        @"   /",
+                                                        @"--/ " };
+        #endregion
+    }
+}
