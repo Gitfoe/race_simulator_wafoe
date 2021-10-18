eb72fc090e216a62c82afbcd67401d98e4ad0dc2
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2021-10-18 20:33:41 +0200

    Level 6-3

---
+++ WpfApp1/Classes/GraphicsCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ViewGraphic.Classes
+{
+    public static class GraphicsCache
+    {
+        // Fields
+        private static Dictionary<string, Bitmap> _cache;
+
+        // Methods
+        public static void Initialize()
+        {
+            _cache = new Dictionary<string, Bitmap>();
+        }
+
+        public static Bitmap GetBitmapData(string imageLocation)
+        {
+            // Checks if the key "imageLocation" exists in the _cache dictionary, and if it does not exist, adds the imageLocation and creates a Bitmap for it
+            if (!_cache.ContainsKey(imageLocation))
+            {
+                _cache.Add(imageLocation, new Bitmap($@"{imageLocation}"));
+            }
+            return _cache[imageLocation];
+        }
+
+        public static void ClearCache()
+        { // Clears the _cache dictionary
+            _cache.Clear();
+        }
+    }
+}
