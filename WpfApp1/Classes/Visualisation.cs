29500db8ec533ac20ec3ec7f89d7fc108be0bce0
Gitfoe <43130920+Gitfoe@users.noreply.github.com>
2021-10-18 21:53:51 +0200

    Level 6-5

---
+++ WpfApp1/Classes/Visualisation.cs
@@ -0,0 +1,22 @@
+﻿using Model.Classes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ViewGraphic.Classes
+{
+    public static class Visualisation
+    {
+        public static BitmapSource DrawTrack(Track track)
+        {
+            return GraphicsCache.CreateBitmapSourceFromGdiBitmap(GraphicsCache.EmptyTrack(256, 256));
+        }
+
+        public static void OnDriversChanged(object sender, DriversChangedEventArgs args)
+        {
+            DrawTrack(args.Track);
+        }
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using Model;
+using Model.Classes;
+using Model.Interfaces;
+using Controller;
+using Controller.Classes;
+using ViewGraphic.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +19,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp1
 {
@@ -23,10 +31,31 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+
+            GraphicsCache.Initialize();
+            Data.Initialize();
+            Data.NextRaceEvent += OnNextRaceEvent; // tell data about visualization event.
+            Data.NextRace();
         }
 
+        private void OnNextRaceEvent(object sender, NextRaceEventArgs args)
+        { // Link events and draw track for the first time
+            args.Race.DriversChanged += Visualisation.OnDriversChanged;
+            GraphicsCache.ClearCache();
+            
+            // Dispatcher priority
+            this.FirstLook.Dispatcher.BeginInvoke(
+                DispatcherPriority.Render,
+                new Action(() =>
+                {
+                    this.FirstLook.Source = null;
+                    this.FirstLook.Source = Visualisation.DrawTrack(args.Race.Track); ;
+                }));
+        }
+
+
         private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        { // ?
 
         }
     }
